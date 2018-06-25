daa3b4ac2c277570f9c960fa26e3fd92cd5d5971
aji <ajianeesh6162@gmail.com>
2018-06-25 23:02:50 +0530

    Hash implemented for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,9 @@ const appRoutes: Routes = [
     InfiniteScrollModule,
     ReactiveFormsModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes,{
+      useHash:true
+    })
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
